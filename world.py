c86e5b400a455e705a04a060e0af848137a3dbb3
askbrid <167693022+askbrid@users.noreply.github.com>
2024-04-21 23:16:16 +0900

    Create world.py

---
+++ world.py
@@ -0,0 +1,8 @@
+class world:
+    def hellow():
+        print("hellow")
+    def missing(Mychild):
+        print("Do not cry.\n{} will be home around this time tomorrow.\nUntil then, there is hope.\n Than he said sorry".format(Mychild))
+      
+world.hellow()
+world.missing("jack")
